59471b1de03f9268dc89fd2ad1e2df32023289b8
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-12-05 23:37:17 +0300

    feat: implemented cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const result = [];
+    argv.forEach((value, index) => {
+        if ((index === 1 || index % 2 === 0) && index > 1) {
+            result.push(`${value.slice(2)} is ${argv[index + 1]}`);
+        }
+    });
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = [];
+
+    const entries = Object.entries(process.env);
+
+    entries.forEach(item => {
+        if (item[0].substring(0, 4) === 'RSS_') result.push(`${item[0]}=${item[1]}`);
+    });
+
+    console.log(result.join('; '));
 };
 
 parseEnv();
