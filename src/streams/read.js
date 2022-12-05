29acf9b134951f42515d2fd1058dfda7e61c84d5
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2022-12-05 23:38:58 +0300

    feat: implemented streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { stdout } from 'process';
+import path from 'path';
+import * as url from 'url';
+import fs from "fs";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const input = fs.createReadStream(path.join(`${__dirname}/files/`, 'fileToRead.txt'));
+    input.pipe(stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { stdin } from 'process';
+import path from 'path';
+import * as url from 'url';
+import fs from "fs";
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const out = fs.createWriteStream(path.join(`${__dirname}/files/`, 'fileToWrite.txt'));
+    stdin.pipe(out);
 };
 
 await write();
